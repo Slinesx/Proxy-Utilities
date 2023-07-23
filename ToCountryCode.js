0aed9c109058e3e718025ab281f9be23ceda1662
Liafonx <79797050+Liafonx@users.noreply.github.com>
2023-07-23 13:19:50 +0800

    Update ToCountryCode.js

--- ToCountryCode.js
+++ ToCountryCode.js
@@ -261,7 +261,7 @@ function operator(proxies) {
 		}
 		let Flag = p.name.match(/[\uD83C][\uDDE6-\uDDFF][\uD83C][\uDDE6-\uDDFF]/)?.[0] || '🏴‍☠️';
 		if (Flag != '🏴‍☠️'){
-			let keywords = ISOFlags[Flag][0];
+			let keywords = ISOFlags[Flag][1];
 			p.name = `${Flag} ${keywords}•|`;
                 }
                 if (!counter[p.name]) counter[p.name] = 0;       
